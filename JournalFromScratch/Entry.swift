d1c885cba141e7979f4b39aaed444443702533f0
Aaron Eliason <aaroneliason@hotmail.com>
2016-04-08 13:43:57 -0600

    Files and Views set up

---
+++ JournalFromScratch/Entry.swift
@@ -0,0 +1,9 @@
+//
+//  Entry.swift
+//  JournalFromScratch
+//
+//  Created by Aaron Eliason on 4/8/16.
+//  Copyright © 2016 IlasonApps. All rights reserved.
+//
+
+import Foundation
---
+++ JournalFromScratch/EntryController.swift
@@ -0,0 +1,9 @@
+//
+//  EntryController.swift
+//  JournalFromScratch
+//
+//  Created by Aaron Eliason on 4/8/16.
+//  Copyright © 2016 IlasonApps. All rights reserved.
+//
+
+import Foundation
