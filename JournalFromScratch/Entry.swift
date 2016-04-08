ed1612b1991ce60ed8f912eb93afbe6796959385
Aaron Eliason <aaroneliason@hotmail.com>
2016-04-08 13:54:07 -0600

    Entry class set up

--- JournalFromScratch/Entry.swift
+++ JournalFromScratch/Entry.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+
+class Entry {
+    private let kTimestamp = "timestamp"
+    private let kTitle = "title"
+    private let kText = "text"
+    
+    
+    var timestamp: NSDate
+    var title: String
+    var text: String
+    
+    init(timestamp: NSDate = NSDate(), title: String, text: String) {
+        self.timestamp = timestamp
+        self.title = title
+        self.text = text
+    }
+    
+    init?(dictionary: [String: AnyObject]) {
+        guard let timestamp = dictionary [kTimestamp] as? NSDate,
+        title = dictionary [kTitle] as? String,
+            text = dictionary [kText] as? String else {return nil}
+        self.timestamp = timestamp
+        self.title = title
+        self.text = text
+    }
+    
+    var dictionaryCopy: [String: AnyObject] {
+        let dictionary = [
+            kTimestamp: self.timestamp,
+            kTitle: self.title,
+            kText: self.text
+            
+        ]
+        return dictionary
+    }
+}
